38ea03e6b3febf872916c1c5cdf369ec6b82a32a
priyamorajkar <fs20if019spoken@gmail.com>
2025-04-12 12:14:13 -0700

    Add files via upload

---
+++ TASK3/spam.py
@@ -0,0 +1,33 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import LogisticRegression
+from sklearn.metrics import classification_report, accuracy_score
+
+# Load the dataset
+df = pd.read_csv("C:/Users/Admin/Documents/CODSOFT ML/TASK4/spam.csv", encoding='latin-1')
+
+
+# Keep only relevant columns
+df = df[['v1', 'v2']]
+df.columns = ['label', 'message']
+
+# Convert labels to binary
+df['label'] = df['label'].map({'ham': 0, 'spam': 1})
+
+# Split the data
+X_train, X_test, y_train, y_test = train_test_split(df['message'], df['label'], test_size=0.2, random_state=42)
+
+# Vectorize the text using TF-IDF
+vectorizer = TfidfVectorizer(stop_words='english')
+X_train_tfidf = vectorizer.fit_transform(X_train)
+X_test_tfidf = vectorizer.transform(X_test)
+
+# Train a Logistic Regression model
+model = LogisticRegression()
+model.fit(X_train_tfidf, y_train)
+
+# Evaluate
+y_pred = model.predict(X_test_tfidf)
+print("Accuracy:", accuracy_score(y_test, y_pred))
+print(classification_report(y_test, y_pred))
